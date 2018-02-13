0436d11a0605a0d5e077f5f2d532cdfe99dc09ed
lhernandezc87 <lhernandez@growthaccelerationpartners.com>
2018-02-13 14:54:28 -0600

    selector example

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,7 +1,9 @@
 import { createSelector } from 'reselect';
+import _ from 'lodash';
 
 const getStores = (state) => state.stores;
 const getShoes = (state) => state.shoes;
+const getSortedStoresColumn = (state) => state.storesSortedColumn;
 
 export const getStoresList = createSelector(
   [getStores],
@@ -14,5 +16,13 @@ export const getShoesList = createSelector(
   [getShoes],
   (shoes) => {
   	return shoes;
+  }
+)
+
+
+export const getSortedStoresList = createSelector(
+  [getStores, getSortedStoresColumn],
+  (stores, sortedColumn) => {
+  	return _.sortBy(stores, [sortedColumn, 'id']);
   }
 )
